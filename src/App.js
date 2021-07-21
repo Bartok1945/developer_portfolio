269c46818e75cafaf76aab40bac3516e392d7a86
Bartok <thom.huenger@gmail.com>
2021-07-21 12:55:23 -0400

    added components to app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-
+import Navbar from "./components/navbar/Navbar"
+import Intro from "./components/intro/Intro"
+import Portfolio from "./components/portfolio/Portfolio"
+import Contact from "./components/contact/Contact"
+import "./app.scss"
 function App() {
   return (
     <div className="app">
-     top
-     sec -intro -portfolio -work ....
+    <Navbar />
+    <div className="sections">
+    <Intro />
+    <Portfolio />
+    <Contact />
+    </div>
+    
     </div>
   );
 }
