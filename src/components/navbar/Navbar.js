a2588a81e554afe4d0ac0826f93d1ac2945a35b0
Bartok <thom.huenger@gmail.com>
2021-07-21 14:30:57 -0400

    working on navbar left side

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,8 +1,26 @@
 import "./navbar.scss"
+import {Mail, Person} from "@material-ui/icons"
 
 export default function Navbar() {
     return (
-        <div className='navbar'>
+        <div className='navbar' id="navbar">
+            <div className="wrapper">
+                <div className="left">
+                    <a href="#intro" className='logo'>THOM</a>
+                    <div className='itemContainer'>
+                    <Mail className='icon' />
+                    <span>thom.huenger@gmail.com</span>
+                    </div>
+                    <div className='itemContainer'>
+                    <Person className='icon' />
+                    <span>301.401.9189</span>
+                    </div>
+                </div>
+              
+                <div className="right">
+                  
+                </div>
+            </div>
 
         </div>
     )
