3d68675f528f0086a892126e5f9d66088d669143
Bartok <thom.huenger@gmail.com>
2021-10-27 21:06:51 -0400

    changes

--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -3,7 +3,19 @@ import "./portfolio.scss"
 export default function Portfolio() {
     return (
         <div className='portfolio' id='portfolio'>
-
+            <h1>Portfolio</h1>
+            <ul>
+                <li>Featured</li>
+                <li>gottaGO</li>
+                <li>Adventure Companion</li>
+                <li>Note Taker</li>
+                <li>Password Generator</li>
+            </ul>
+            <div className='container'>
+                <div className='item'>
+                    <img className="burgerapp" src="assets/burgerapp.PNG" alt="handsome man"></img>
+                </div>
+            </div>
         </div>
     )
 } 
